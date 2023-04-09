f896e13cba193978761b216dbb736e77c889839d
Vinicius Antunes <viniciusantunes@ufpr.br>
2023-04-09 17:43:50 -0300

    novo pedido +-

---
+++ js/novo_pedido.js
@@ -0,0 +1,63 @@
+const orcamento = [
+    
+];
+
+
+const form_login = document.querySelector("#form_pedido");
+const selectItems = document.querySelector("#pecas");
+
+const items = [
+    {id: 1, nome: "camiseta", valor: "10"},
+    {id: 2,nome: "calcas", valor: "12"},
+    {id: 3,nome: "intimos", valor: "15"},
+];
+
+
+for(item in items) {
+    option = new Option(items[item].nome, items[item].valor);
+    option.dataset.prazo = items[item].prazo;
+    option.dataset.id = items[item].id;
+    selectItems.options[selectItems.options.length] = option;
+}
+
+
+form_login.onsubmit = event =>{
+    event.preventDefault();}
+
+//botao adicionar itens ao orçamento
+
+// const addItemsBtn = document.getElementById("btn-item");
+// addItemsBtn.addEventListener('click',adicionarItem);
+
+
+var prod = "";
+function adicionarItem(){
+
+    var qtd = document.getElementById("qtd_pecas").value;
+    var select = document.getElementById("pecas"); //select
+    var idx = select.selectedIndex;
+    prod = select.options[idx];
+    console.log(prod);
+
+    if(orcamento.length > 0){ // já tem algo no orçamento
+
+    }else{ // novo
+        var indx;
+        var table = document.getElementById("tbl");
+        var row = table.insertRow(table.rows.length);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        cell1.innerHTML = prod.text;
+        cell2.innerHTML = qtd;
+        cell3.innerHTML = prod.value;
+        cell4.innerHTML = "<button class='btn btn-danger'onclick='removeTableRow(this)'>Excluir</button>"
+    }
+
+}
+
+function removeTableRow(row) {
+    var tableRow = row.parentNode.parentNode; // obtém a linha da tabela a partir do botão que foi clicado
+    tableRow.parentNode.removeChild(tableRow); // remove a linha da tabela
+}
