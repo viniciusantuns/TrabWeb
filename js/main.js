50ea6d3b8d787c902fd17b58b0d38aba597d808a
NatanCervinski <natanscer@gmail.com>
2023-04-17 02:13:04 -0300

    adicionado confimação de recolhimento, mas n muito

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var estados = [
+	{id: 1, estado: "EM ABERTO", cor: "FFFF00"},
+	{id: 2, estado: "REJEITADO/CANCELADO", cor: "FF0000"},
+	{id: 3, estado: "RECOLHIDO", cor: "808080"},
+	{id: 4, estado: "AGUARDANDO PAGAMENTO", cor: "0080FF"},
+	{id: 5, estado: "PAGO", cor: "FF8000"},
+	{id: 6, estado: "FINALIZADO", cor: "00CC00"},
+];
