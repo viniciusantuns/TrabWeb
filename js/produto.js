fc625f46c1c769a59a494b0cf7755286dd665748
JessSouzaUFPR <fatimajessica2@gmail.com>
2023-04-25 01:07:35 -0300

    inclui as telas do produto, e o escopo js

---
+++ js/produto.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    listarProdutos('',produtos);
+});
+
+
+const produto = [
+    {id: 1, nome: "camiseta", valor: "10", prazo: 1},
+    {id: 2, nome: "camisa", valor: "20",  prazo: 3},
+    {id: 2,nome: "calcas", valor: "12",  prazo: 2},
+    {id: 3,nome: "cueca", valor: "15",  prazo: 1},
+    {id: 5,nome: "meia", valor: "9",  prazo: 1},
+];
+
+
+function listarProdutos(){
+
+
+}
+
+function adicionarProduto(){
+
+
+}
+
+function editarProduto(){
+    
+}
+
+function excluirProduto(){
+    
+}
