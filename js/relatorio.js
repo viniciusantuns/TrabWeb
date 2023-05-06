efb6e56e7cffa1c71d6ff6cc965245bb7daa45fa
cristhian <cristhianrodriques@gmail.com>
2023-05-06 13:08:11 -0300

    Ateração dos relatorios para JSPDF

--- js/relatorio.js
+++ js/relatorio.js
@@ -1,42 +1,24 @@
-function relcliente_pdf() {
-
-    var dadoscliente = '<p style="text-align:center;">RELATÓRIO DE CLIENTES <\p> <hr>  <p style="font-weight: bold;"> Nome: Claudio da Silva Sauro <\p>  <hr>  CPF: 102.450.340-99    UF: PR         Cidade: Curitiba  <hr>  Rua: Michale Jackson        Numero:300      <hr>    Email: Claudiosaouro22@gmaill.com <br> <hr><br> <p style="font-weight: bold;"> Nome: Ana Claudia <\p>  <hr>  CPF: 102.450.340-99    UF: PR         Cidade: Curitiba  <hr>  Rua: Maicon Jaquison        Numero:300      <hr>    Email: v1d4l0k422@gmaill.com <br><hr><br> <p style="font-weight: bold;"> Nome: Claudia da Silva Sauro <\p>  <hr>  CPF: 102.450.340-99    UF: PR         Cidade: Curitiba  <hr>  Rua: Michale Jackson        Numero:300      <hr>    Email: Claudiasaouro22@gmaill.com <br>'
-    var janela = window.open('', '', 'width=500,heigth=200');
-    janela.document.write('<html><head>');
-    janela.document.write('<title>Relatorio de Clientes</title></head>');
-    janela.document.write('<body>');
-    janela.document.write(dadoscliente);
-    janela.document.write('</body></html>');
-    janela.document.close();
-    janela.print();
-}
-function reltopcliente_pdf() {
-
-
-    var dadosclientetop = '<p style="text-align:center;">RELATÓRIO DE CLIENTES TOP <\p> <hr>  <p style="font-weight: bold;"> Nome: Claudio da Silva Sauro <\p>  <hr>  CPF: 102.450.340-99    UF: PR         Cidade: Curitiba  <hr>  Rua: Michale Jackson        Numero:300      <hr>    Email: Claudiosaouro22@gmaill.com <br> <br> Total Pedido 1 = R$50,00 <br> Total Pedido 2 = R$80,00 <br> Total Pedido 3 = R$100,00 <br> <p style="text-align:end;"> Total: R$230,00 <\p> <hr><br> <p style="font-weight: bold;"> Nome: Ana Cristina <\p>  <hr>  CPF: 102.450.340-99    UF: PR         Cidade: Curitiba  <hr>  Rua: Maicon Jaquison    Numero:300      <hr>    Email: v1d4l0k422@gmaill.com <br> Total Pedido 1 = R$50,00 <br> Total Pedido 2 = R$80,00 <br> Total Pedido 3 = R$200,00 <br> <p style="text-align:end;"> Total: R$330,00 <\p> <br><hr><br> <p style="font-weight: bold;"> Nome: Claudia da Silva Sauro <\p>  <hr>  CPF: 102.450.344-99    UF: PR         Cidade: Curitiba  <hr>  Rua: Michale Jackson        Numero:300      <hr>    Email: Claudiasaouro22@gmaill.com <br> Quantidade de pedidos : 3 <br> Total Pedido 1 = R$50,00 <br> Total Pedido 2 = R$80,00 <br> Total Pedido 3 = R$50,00 <br> <p style="text-align:end;"> Total: R$180,00 <\p>'
-    var janela = window.open('', '', 'width=500,heigth=200');
-    janela.document.write('<html><head>');
-    janela.document.write('<title>Relatorio de Clientes TOP</title></head>');
-    janela.document.write('<body>');
-    janela.document.write(dadosclientetop);
-    janela.document.write('</body></html>');
-    janela.print();
-    janela.document.close();
-    window.close();
-}
-
 function relceita_pdf() {
 
 
-    var dadosrelceita = '<p style="text-align:center;">RECEITA POR PERÍODO <br> Periodo:xx/xx/xx á xx/xx/xx<\p> <hr>  <p style="text-align: end;"> Quantidade de lavagens 5 <\p> <br> Lavagen 1      01/02/2023<br>    1 Calça  R$40,00     |     1 Meia R$10,00     |     Total: 50R$    <hr> Lavagen 2      10/03/2023<br> 1 Calça  R$40,00     |     3 Camisa R$30,00     |     Total: 120R$   <hr> Lavagen 3      12/04/2023<br>    2 Calça  R$40,00     |     1 Camisa R$30,00     |     Total: 110R$    <hr> Lavagen 4      15/05/2023<br> 1 Calça  R$40,00     |     2 Camisa R$30,00     |     Total: 100R$    <hr> Lavagen 5      20/10/2023<br> 1 Calça  R$40,00     |     1 Camisa R$30,00     |     Total: 70R$    <hr><p style="text-align:end;">Total = R$450,00 <\p>  '
-    var janela = window.open('', '', 'width=500,heigth=200');
-    janela.document.write('<html><head>');
-    janela.document.write('<title>Relatorio de Clientes TOP</title></head>');
-    janela.document.write('<body>');
-    janela.document.write(dadosrelceita);
-    janela.document.write('</body></html>');
-    janela.document.close();
-    janela.print();
+    var dadosrelceita = '                                       RELATÓRIO DE RECEITA \n                                     Periodo:xx/xx/xx á xx/xx/xxxx \n\n\nQuantidade de lavagens 5 \n\nLavagen 1      01/02/2023\n1 Calça  R$40,00     |     1 Meia R$10,00     |     Total: 50R$    \n\nLavagen 2      10/03/2023 \n1 Calça  R$40,00     |     3 Camisa R$30,00     |     Total: 120R$   \n\nLavagen 3      12/04/2023    \n2 Calça  R$40,00     |     1 Camisa R$30,00     |     Total: 110R$    \n\nLavagen 4      15/05/2023 \n1 Calça  R$40,00     |     2 Camisa R$30,00     |     Total: 100R$    \n\nLavagen 5      20/10/2023 \n1 Calça  R$40,00     |     1 Camisa R$30,00     |     Total: 70R$    \n\n                                                                                      Total = R$450,00 '
+    var doc = new jsPDF();
+    doc.text(dadosrelceita,10,10)
+    doc.save('RelatoriodeReceita.pdf');
 }
 
+function relcliente_pdf()
+{
+    var dadoscliente = '                                       RELATÓRIO DE CLIENTES \n\n\nNome: Claudio da Silva Sauro \nCPF: 102.450.340-99    UF: PR         Cidade: Curitiba  \nRua: Michale Jackson        Numero:300      \nEmail: Claudiosaouro22@gmaill.com \n \nNome: Ana Claudia  \nCPF: 102.450.340-99    UF: PR         Cidade: Curitiba  \nRua: Maicon Jaquison        Numero:300      \nEmail: v1d4l0k422@gmaill.com \n \nNome: Claudia da Silva Sauro  \nCPF: 102.450.340-99    UF: PR         Cidade: Curitiba  \nRua: Michale Jackson        Numero:300    \nEmail: Claudiasaouro22@gmaill.com'
+   var doc = new jsPDF();
+   doc.text(dadoscliente,10,10)
+   doc.save('RelatoriodeCLientes.pdf');
+}
 
+function reltopcliente_pdf() {
+
+
+    var doc = new jsPDF();
+    doc.text('                                       RELATÓRIO DE CLIENTES TOP \n\n\nNome: Claudio da Silva Sauro \nCPF: 102.450.340-99    UF: PR         Cidade: Curitiba  \nRua: Michale Jackson        Numero:300      \nEmail: Claudiosaouro22@gmaill.com \n\nTotal Pedido 1 = R$50,00 \nTotal Pedido 2 = R$80,00 \nTotal Pedido 3 = R$100,00 \n                                                                                            Total: R$230,00  \n\n\nNome: Ana Cristina \nCPF: 102.450.340-99    UF: PR         Cidade: Curitiba  \nRua: Maicon Jaquison    Numero:300      \nEmail: v1d4l0k422@gmaill.com \n\nTotal Pedido 1 = R$50,00 \nTotal Pedido 2 = R$80,00 \nTotal Pedido 3 = R$200,00 \n                                                                                            Total: R$330,00 \n\n\nNome: Claudia da Silva Sauro \nCPF: 102.450.344-99    UF: PR         Cidade: Curitiba  \nRua: Michale Jackson        Numero:300      \nEmail: Claudiasaouro22@gmaill.com \nQuantidade de pedidos : 3 \n\nTotal Pedido 1 = R$50,00 \nTotal Pedido 2 = R$80,00 \nTotal Pedido 3 = R$50,00 \n                                                                                            Total: R$180,00 ',10,10)
+    doc.save('RelatorioClienteTop.pdf');
+}
