2a30a8a4a1028f2587c02624e1944b3e6aa6980b
Vinicius Antunes <viniciusantunes@ufpr.br>
2023-06-24 16:20:32 -0300

    crud produto

--- src/app/funcionario/modal-produto/modal-produto.component.ts
+++ src/app/funcionario/modal-produto/modal-produto.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, ViewChild, EventEmitter  } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Produto } from 'src/app/shared/models/produto.model';
 import { FuncionarioService } from '../service/funcionario.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-produto',
@@ -12,15 +13,26 @@ export class ModalProdutoComponent {
 
   @Input() produto!: Produto;
   @Input() acao!: string;
+
+  @Output() salvarProdutoEvento = new EventEmitter<Produto>();
+  @Output() editarProdutoEvento = new EventEmitter<Produto>();
+
+  @ViewChild('formProduto') formProduto! : NgForm; 
+
   constructor(public activeModal: NgbActiveModal, private funcionarioService: FuncionarioService) {}
 
-
   salvar(){
-
+    if(this.formProduto.form.valid){
+      this.salvarProdutoEvento.emit(this.produto);
+      this.activeModal.close()
+    }
   }
 
   editar(){
-
+    if(this.formProduto.form.valid){
+      this.salvarProdutoEvento.emit(this.produto);
+      this.activeModal.close()
+    }
   }
   
 }
